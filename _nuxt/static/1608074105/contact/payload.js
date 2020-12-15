fe600cf3601c64a56f84f4b968e3778692b7bb94
slatermatt <matthewslater1998@gmail.com>
2020-12-15 23:15:20 +0000

    Deploying to gh-pages from  @ 450c5c04f0df9b2841320efe8a45e7f498386c22 🚀

---
+++ _nuxt/static/1608074105/contact/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/contact", (function(a){return {data:[{slug:"contact",title:"Contact",model:{intro:{title:"Let's talk",content:"Give us a ring, or fill out the form below and we'll get in touch with you as soon as possible."}},dir:"\u002F",path:"\u002Fcontact",extension:".json",createdAt:a,updatedAt:a}],fetch:[],mutations:[]}}("2020-12-15T23:14:16.437Z")));
