351ee346f4956c9d78a829930f9c07c4c1e3f8b7
Matt Slater <matthewslater1998@gmail.com>
2021-02-19 11:41:24 +0000

    Updates landing page

--- resources/views/layouts/simple.vue
+++ resources/views/layouts/simple.vue
@@ -1,10 +1,23 @@
 <template>
 	<div
 		v-cloak
-		class="flex flex-col min-h-screen"
+		class="flex flex-col min-h-screen bg-brand"
 	>
 		<main class="relative flex-auto z-site-content">
-			<Nuxt />
+			<div class="relative z-1">
+				<Nuxt />
+			</div>
+
+			<div class="opacity-50 hidden lg:block">
+				<video
+					class="absolute inset-0 w-full h-full object-cover"
+					src="/static/img/globe.mov"
+					playsinline
+					autoplay
+					muted
+					loop
+				/>
+			</div>
 		</main>
 	</div>
 </template>
--- resources/views/pages/index.vue
+++ resources/views/pages/index.vue
@@ -1,20 +1,21 @@
 <template>
 	<div class="x-container flex flex-col items-center justify-center min-h-screen h-full">
-		<logo />
+		<div class="max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-5xl">
+			<logo />
 
-		<h1
-			:class="[
-				'text-center max-w-2xl x-h1',
-				'xl:max-w-3xl',
-				'mt-6 md:mt-8 xl:mt-10',
-			]"
-			v-text="$data.model.title"
-		/>
+			<h1
+				:class="[
+					'x-h1 x-fade mt-2',
+					'xl:mt-5',
+				]"
+				v-text="$data.model.title"
+			/>
 
-		<x-button
-			class="mt-8 md:mt-10 xl:mt-12"
-			v-bind="$data.model.cta"
-		/>
+			<x-button
+				class="mt-8 md:mt-10 xl:mt-12"
+				v-bind="$data.model.cta"
+			/>
+		</div>
 	</div>
 </template>
 
