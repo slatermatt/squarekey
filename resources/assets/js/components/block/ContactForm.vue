6aabd807f740a550479c85cc23e736520b0592b4
Matt Slater <matthewslater1998@gmail.com>
2021-02-19 15:05:08 +0000

    Adds contact form style

--- resources/assets/js/components/block/ContactForm.vue
+++ resources/assets/js/components/block/ContactForm.vue
@@ -1,14 +1,32 @@
 <template>
-	<form action="/">
-		<div
-			v-for="(field, index) in $props.fields"
-			:key="index"
+	<div class="x-container">
+		<form
+			action="https://formspree.io/f/mbjpeppq"
+			method="POST"
+			class="flex flex-col space-y-5 max-w-xl mx-auto"
 		>
-			<label>
-				<input v-bind="field">
-			</label>
-		</div>
-	</form>
+			<div
+				v-for="(field, index) in $props.fields"
+				:key="index"
+				class="flex flex-col w-full space-y-1"
+			>
+				<label
+					:for="field.id"
+					v-text="field.placeholder"
+				/>
+
+				<input
+					class="px-4 py-3 border"
+					v-bind="field"
+				>
+			</div>
+
+			<x-button
+				title="Submit"
+				class="self-center"
+			/>
+		</form>
+	</div>
 </template>
 
 <script>
